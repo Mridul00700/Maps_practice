b7e82a5df1c1982287c4d99280d4c64bd825898a
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-07-01 12:35:24 +0530

    FormHandling

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
+let map;
+let mapEvent;
 // Geolocation -- Two callbacks -->>
 if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -22,7 +23,7 @@ if (navigator.geolocation)
 
         const coords = [latitude, longitude];
 
-        const map = L.map('map').setView(coords, 13);
+        map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -30,16 +31,31 @@ if (navigator.geolocation)
 
 
 
-        // console.log(map);
-        map.on('click', function (mapEvent) {
-            // console.log(mapEvent);
-            const { lat, lng } = mapEvent.latlng;
-            // All the methods return this hence chainable...
-            L.marker([lat, lng]).addTo(map)
-                .bindPopup(L.popup({ maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: 'running-popup' }))
-                .setPopupContent("Workout").openPopup();
-        });
+        // console.log(map); Handling clicks on map
+        map.on('click', function (mapE) {
+            mapEvent = mapE;
+            form.classList.remove('hidden');
+            inputDistance.focus();
 
-    }, function () {
-        alert("Could not get your position");
+
+        }, function () {
+            alert("Could not get your position");
+        })
     });
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // Clear Input fields 
+    inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value = "";
+
+    // Submit the marker
+    console.log(mapEvent);
+    const { lat, lng } = mapEvent.latlng;
+    // All the methods return this hence chainable...
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(L.popup({ maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: 'running-popup' }))
+        .setPopupContent("Workout").openPopup();
+});
+
