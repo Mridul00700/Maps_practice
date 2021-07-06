5fcb093981c6c2c090e7609fea023bee4ce58968
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-07-06 12:02:46 +0530

    Custom_HTML-Adjusted-And_Inserted-IN-DOM

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 
 //  OOP Implementation...
 // Implementing App Class...>>>
@@ -18,7 +17,14 @@ class Workout {
     constructor(coords, distance, duration) {
         this.coords = coords;  // [] of coordinated [lat, lng]
         this.distance = distance;
-        this.duration = duration
+        this.duration = duration;
+    }
+
+    _setDescription() {
+
+        // prettier-ignore
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
     }
 }
 
@@ -28,6 +34,8 @@ class Running extends Workout {
         super(coords, distance, duration);
         this.cadence = cadence;
         this.calcPace();
+        this._setDescription();
+
     }
 
     calcPace() {
@@ -42,6 +50,8 @@ class CyCling extends Workout {
         super(coords, distance, duration);
         this.elevationGain = elevationGain;
         this.calcSpeed();
+        this._setDescription();
+
     }
     calcSpeed() {
         //  Km / Hr
@@ -192,23 +202,51 @@ class App {
 
     _renderWorkout(workout) {
 
-
-        const html = `
-        <li class="workout workout--${workout.name}" data-id="${workout.id}">
-          <h2 class="workout__title">Running on April 14</h2>
+        let html = `
+        <li class="workout workout--${workout.type}" data-id="${workout.id}">
+          <h2 class="workout__title">${workout.description}</h2>
           <div class="workout__details">
-            <span class="workout__icon">${workout.name === 'running' ? '🏃‍♂️' : '🚴‍♀️'}</span>
+            <span class="workout__icon">${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'}</span>
             <span class="workout__value">${workout.distance}</span>
             <span class="workout__unit">km</span>
           </div >
-    <div class="workout__details">
-        <span class="workout__icon">⏱</span>
-        <span class="workout__value">${workout.duration}</span>
-        <span class="workout__unit">min</span>
-    </div>
-`
-    }
+          <div class="workout__details">
+          <span class="workout__icon">⏱</span>
+          <span class="workout__value">${workout.duration}</span>
+          <span class="workout__unit">min</span>
+          </div>
+        `;
+        if (workout.type === 'running') {
+            html += `
+            <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.pace.toFixed(1)}</span>
+            <span class="workout__unit">min/km</span>
+            </div>
+            <div class="workout__details">
+            <span class="workout__icon">🦶🏼</span>
+            <span class="workout__value">${workout.cadence}</span>
+            <span class="workout__unit">spm</span>
+            </div>
+        </li>`
+        }
+        if (workout.type === 'cycling') {
+            html += `
+            <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+            </div>
+            <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+            </div>
+        </li>`
+        }
 
+        form.insertAdjacentHTML('afterend', html);
+    }
 }
 
 
